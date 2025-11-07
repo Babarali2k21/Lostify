2261ce05de27cd1818ed5f00ad6ef7a4cf93d0f7
Babar Ali <148423037+Babarali2k21@users.noreply.github.com>
2025-11-07 14:11:59 +0100

    corrected some typos and functions

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,12 +1,12 @@
-import { createServerClinet } from '@supabase/ssr'
+import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export async function createClient() {
   const cookieStore = cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll() {
