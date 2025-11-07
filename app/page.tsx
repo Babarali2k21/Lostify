1cb30679c75c1d4f6f5c73b7e509b49baca8ca0b
Babar Ali <148423037+Babarali2k21@users.noreply.github.com>
2025-11-07 14:39:41 +0100

    connect Home page to Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <p className="flex items-center justify-center min-h-screen">Welcome to Lostify!</p>;
+import { createClient } from '@/lib/supabase/server';
+
+export default async function Home() {
+  const supabase = await createClient();
+  const { data: instruments } = await supabase.from("instruments").select();
+  console.log(instruments);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Welcome to Lostify!</h1>
+      <ul>
+        {instruments.map((instrument) => (
+          <li key={instrument.id}>{instrument.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
